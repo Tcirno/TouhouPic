0c6e6ff9267d83e0a7e77e3a1ed145b080b3abec
Tcirno <1719655027@qq.com>
2019-11-28 14:12:44 +0800

    v1.4更新mp3的音乐

---
+++ TouhouMp3.py
@@ -0,0 +1,63 @@
+# -*- coding: UTF-8 -*-
+
+import requests
+from bs4 import BeautifulSoup
+import codecs
+import chardet
+import re
+import time
+import random
+import os
+
+import sys
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 '
+                  'Safari/537.36',  # 构建用户代理
+    'Connection': 'close',  # 拿到网页内容后关闭连接
+}
+
+
+root = 'https://thwiki.cc'
+# rooturl = 'https://thwiki.cc/%E6%96%87%E4%BB%B6:'
+rooturl = 'https://thwiki.cc/文件:'
+saveturl = './thout/TouhouMp3/'
+
+#从touhouwiki上面获取所有mmp3音乐
+for i in range(6, 18):
+    for j in range(1, 30):
+        if j < 10:
+            url = rooturl + 'th0' + str(i) + '_' + '0' + str(j) + '.mp3'
+            filename = 'th0' + str(i) + '_' + '0' + str(j) + '.mp3'
+            html = requests.get(url, headers=header)
+            html.encoding = chardet.detect(html.content)['encoding']
+            soup = BeautifulSoup(html.text, "html.parser")
+            my = saveturl + 'th0' + str(i) + '0' + str(j) + '.html'
+            data = soup.findAll(name='a', attrs={"href": re.compile(r'(\/\w{1,2}){2}\/')})
+            if str(data) == '[]':
+                continue
+            mp3url = data[0].get('href')
+            f = open(saveturl + filename, 'wb')
+            f.write(requests.get(mp3url).content)
+            f.close()
+
+        else:
+            url = rooturl + 'th0' + str(i) + '_' + str(j) + '.mp3'
+            filename = 'th0' + str(i) + '_' + str(j) + '.mp3'
+            html = requests.get(url, headers=header)
+            html.encoding = chardet.detect(html.content)['encoding']
+            soup = BeautifulSoup(html.text, "html.parser")
+            my = saveturl + 'th0' + str(i) + str(j) + '.html'
+            data = soup.findAll(name='a', attrs={"href": re.compile(r'(\/\w{1,2}){2}\/')})
+            if str(data) == '[]':
+                continue
+            mp3url = data[0].get('href')
+            f = open(saveturl + filename, 'wb')
+            f.write(requests.get(mp3url).content)
+            f.close()
+        time.sleep(random.randint(0, 2))
+
+quit(0)
